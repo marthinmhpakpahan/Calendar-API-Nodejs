0ce3db9310826505b9bdfb0a7d0cf41c3068431c
marthinmhpakpahan <marthinmhpakpahan@gmail.com>
2018-11-28 10:27:54 +0000

    allow cors

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  next();
+});
+
 // USER ROUTES
 var UserController = require("./user/UserController");
 app.use("/users", UserController);
